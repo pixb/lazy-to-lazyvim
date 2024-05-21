fe420eb92f75010eab403cfb858fb3bda8b9ce9f
pix <tpxsky@163.com>
2024-05-21 20:59:05 +0800

    open number

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
 vim.api.nvim_set_keymap('i', 'jk', '<ESC>', { noremap = true, silent = true })
--- vim.o.number = true
--- vim.o.relativenumber = true
+vim.o.number = true
+vim.o.relativenumber = true
 require("config.lazy")

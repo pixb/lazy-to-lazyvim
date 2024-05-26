5788a2b0057685010b8f34f70b4219acca110908
pix <tpxsky@163.com>
2024-05-27 04:49:22 +0800

    add coding plugin

--- lua/lazyvim/util/init.lua
+++ lua/lazyvim/util/init.lua
@@ -158,4 +158,11 @@ function M.safe_keymap_set(mode, lhs, rhs, opts)
   end
 end
 
+M.CREATE_UNDO = vim.api.nvim_replace_termcodes("<c-G>u", true, true, true)
+function M.create_undo()
+  if vim.api.nvim_get_mode().mode == "i" then
+    vim.api.nvim_feedkeys(M.CREATE_UNDO, "n", false)
+  end
+end
+
 return M
